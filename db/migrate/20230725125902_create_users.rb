2e4c433fdf209ab620d1e84f64789262548d0658
Samuel <samnmbuguah@gmail.com>
2023-07-25 16:02:19 +0300

    Generate users scaffold

---
+++ db/migrate/20230725125902_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.integer :social_media_id
+      t.string :profile_picture
+      t.string :social_media_provider
+      t.boolean :is_admin
+      t.boolean :is_moderator
+
+      t.timestamps
+    end
+  end
+end
