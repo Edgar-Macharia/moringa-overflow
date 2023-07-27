f3146901ba0d99b91cd377597f91136b8d563f51
Samuel <samnmbuguah@gmail.com>
2023-07-28 00:26:14 +0300

    Jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,31 @@
-class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
-
+require 'jwt'
+class ApplicationController < ActionController::API
+    # skip_before_action :verify_authenticity_token
+      before_action :authorize_request
+    
+      private
+    
+      def authorize_request
+            header = request.headers['Authorization']
+            if header.present?
+              token = header.split(' ').last
+              begin
+                decoded = JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' }).first
+                @current_user = User.find(decoded['user_id'])
+                if decoded['exp'].to_i < Time.now.to_i
+                  render json: { error: 'Token has expired' }, status: :unauthorized
+                end
+              rescue JWT::DecodeError
+                render json: { error: 'Invalid token' }, status: :unauthorized
+              rescue ActiveRecord::RecordNotFound
+                render json: { error: 'User not found' }, status: :unauthorized
+              end
+            else
+              render json: { error: 'Missing token' }, status: :unauthorized
+            end
+      end
+          
+      def current_user
+        @current_user
+      end
 end
