c25dee1a1cc6ebb44e07406cb8ae43be7c27fc0d
Samuel <samnmbuguah@gmail.com>
2023-08-01 00:15:02 +0300

    questions serializer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   # GET /questions or /questions.json
   def index
     @questions = Question.all
-    render json: @questions
+    render json: @questions, each_serializer: QuestionSerializer, include: 'answers'
   end
 
   # GET /questions/1 or /questions/1.json
