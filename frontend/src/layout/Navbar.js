71d264bf3a03b876b055d4a92c0206db60b836b7
EdgarMacharia <m.edgarmacharia@gmail.com>
2023-07-31 18:06:42 +0300

    changes to navbar

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -14,44 +14,40 @@ const Navbar = () => {
           <Link to="/" class="flex items-center">
             <img src={logo} class="h-20 w-20 rounded-full mr-3" alt="Moringa Overflow Logo" />
           </Link>
-          <div class="flex items-center md:order-2">
-            {isLoggedIn ? (
-              <>
-                 <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                  <span class="sr-only">Open user menu</span>
-                  <img class="w-14 h-14 rounded-full" src={userIcon} alt="user photo" />
-                </button>
-                <Link
-                  to="/profile"
-                  class="px-4 py-3"
-                >
-                  <span class="block text-sm text-gray-900 dark:text-white">Profile</span>
-                  <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{username}</span>
-                </Link>
-                <button
-                  onClick={logout}
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Sign out
-                </button>
-              </>
-            ) : (
-              <>
-                <Link
-                  to="/login"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Login
-                </Link>
-                <Link
-                  to="/signup"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Sign up
-                </Link>
-              </>
-            )}
-          </div>
+          <div className="flex items-center md:order-2">
+      <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+        <span className="sr-only">Open user menu</span>
+        <img className="w-14 h-14 rounded-full" src={userIcon} alt="user photo" />
+      </button>
+      {/* <!-- Dropdown menu --> */}
+      {isLoggedIn ? (
+        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+          <Link to="/profile" className="px-4 py-3">
+            <span className="block text-sm text-gray-900 dark:text-white">Profile</span>
+            <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{username}</span>
+          </Link>
+          <ul className="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <a onClick={logout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+          <ul className="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <Link to="/login" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Login</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+      <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
+    </div>
 
           <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
           <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
