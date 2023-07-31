13dbf5396442aa70ad5947682318cbf458b2c194
EdgarMacharia <m.edgarmacharia@gmail.com>
2023-07-31 17:53:07 +0300

    updated navbar

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -14,44 +14,40 @@ const Navbar = () => {
           <Link to="/" class="flex items-center">
             <img src={logo} class="h-20 w-20 rounded-full mr-3" alt="Moringa Overflow Logo" />
           </Link>
-          <div class="flex items-center md:order-2">
-            {isLoggedIn ? (
-              <>
-                 <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                  <span class="sr-only">Open user menu</span>
-                  <img class="w-14 h-14 rounded-full" src={userIcon} alt="user photo" />
-                </button>
-                <Link
-                  to="/profile"
-                  class="px-4 py-3"
-                >
-                  <span class="block text-sm text-gray-900 dark:text-white">Profile</span>
-                  <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{username}</span>
-                </Link>
-                <button
-                  onClick={logout}
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Sign out
-                </button>
-              </>
-            ) : (
-              <>
-                <Link
-                  to="/login"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Login
-                </Link>
-                <Link
-                  to="/signup"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Sign up
-                </Link>
-              </>
-            )}
-          </div>
+          <div className="flex items-center md:order-2">
+      <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+        <span className="sr-only">Open user menu</span>
+        <img className="w-14 h-14 rounded-full" src={userIcon} alt="user photo" />
+      </button>
+      {/* <!-- Dropdown menu --> */}
+      {isLoggedIn ? (
+        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+          <Link to="/profile" className="px-4 py-3">
+            <span className="block text-sm text-gray-900 dark:text-white">Profile</span>
+            <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{username}</span>
+          </Link>
+          <ul className="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <a onClick={logout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+          <ul className="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <Link to="/login" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Login</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+      <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
+    </div>
 
           <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
           <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -85,7 +81,7 @@ const Navbar = () => {
 
               </li>
               <li>
-                <Link to="/Notifications" class="">
+                <Link to="/Notifications" class="animate-bounce">
                   <svg class="w-6 h-6 text-gray-800 dark:text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z" />
                   </svg>
