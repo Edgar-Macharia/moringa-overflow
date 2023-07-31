9a80a6497acb11b6766c893545d6e67b5d21f3c3
EdgarMacharia <m.edgarmacharia@gmail.com>
2023-07-31 19:12:32 +0300

    changes to signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -98,24 +98,14 @@ const Signup = () => {
                   value={password_confirmation}
                 />
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                  </div>
-                </div>
-    
-              </div>
+
               <button
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Sign up
               </button>
-              <div className='my-5'>Already Registered? 
-                <Link className='ml-4' to="/login">
+              <div className='text-sm font-light text-gray-500 dark:text-gray-400'>Already Registered?  <Link className='font-medium text-primary-600 hover:underline dark:text-primary-500' to="/login">
                   login
                 </Link>
               </div>
