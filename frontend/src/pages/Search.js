c6917e7209ec346e728cebae8865693fac7ee5da
kk <87941966+kelvinkirui@users.noreply.github.com>
2023-08-01 21:33:37 +0300

    updated search functionality

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,9 +1,76 @@
-import React from 'react'
+// import React,{useState} from 'react'
+// import Questions from "./Questions";
+// import Tags from "./Tags";
+// import Navbar from '../layout/Navbar';
+// const Search = () => {
+//   const [searchQuery, setSearchQuery] = useState("");
+//   const filteredQuestions = Questions.filter((question) =>
+//       question.title.toLowerCase().includes(searchQuery.toLowerCase())
+//       );
+//   const filteredTags = Tags.filter((tag) =>
+//     tag.name.toLowerCase().includes(searchQuery.toLowerCase())
+//   );
+//   return (
+//         <div>
+//         {/* Pass the searchQuery and setSearchQuery to Navbar */}
+//         <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+    
+//         {/* Render Questions and Tags with filtered data */}
+//         <Questions data={filteredQuestions} />
+//         <Tags data={filteredTags} />
+//         </div>
+//       );
+//     }
+// export default Search
+// const Search = () => {
+//   const [searchQuery, setSearchQuery] = useState("");
 
-const Search = () => {
+//   // Step 2: Filter Questions and Tags based on the search query
+//   const filteredQuestions = Questions.filter((question) =>
+//     question.title.toLowerCase().includes(searchQuery.toLowerCase())
+//   );
+//   const filteredTags = Tags.filter((tag) =>
+//     tag.name.toLowerCase().includes(searchQuery.toLowerCase())
+//   );
+//   return (
+//     <div>
+//     {/* Pass the searchQuery and setSearchQuery to Navbar */}
+//     <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
+//     {/* Render Questions and Tags with filtered data */}
+//     <Questions data={filteredQuestions} />
+//     <Tags data={filteredTags} />
+//     </div>
+//   );
+// }
+
+// export default Search
+import React,{useState} from "react";
+
+
+const Search = ({ searchQuery }) => {
+  // This is a placeholder array for demonstration purposes.
+  // Replace this with your actual data that needs to be filtered based on the search query.
+  const searchData = [
+    { title: "Question 1" },
+    { title: "Question 2" },
+    { title: "Tag 1" },
+    { title: "Tag 2" },
+  ];
+
+  // Step 4: Implement the search logic to filter the data based on the search query
+  const filteredData = searchData.filter((item) =>
+    item.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div>Search</div>
-  )
-}
+    <div>
+      {/* Display the filtered data */}
+      {filteredData.map((item) => (
+        <div key={item.title}>{item.title}</div>
+      ))}
+    </div>
+  );
+};
 
-export default Search
+export default Search;
