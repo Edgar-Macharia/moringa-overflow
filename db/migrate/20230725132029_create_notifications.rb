16c9a2c82e6c33a6bbfaf3b06197baa2761db812
kk <87941966+kelvinkirui@users.noreply.github.com>
2023-08-03 02:44:47 +0300

    updates

--- db/migrate/20230725132029_create_notifications.rb
+++ db/migrate/20230725132029_create_notifications.rb
@@ -1,11 +1,9 @@
 class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
     create_table :notifications do |t|
-      t.references :user,  null: false, foreign_key: true
-      t.string :notification_type
-      t.boolean :is_read
+      t.string :message
+      t.integer :user_id
+      t.boolean :read_status, default: false
 
       t.timestamps
     end
-  end
-end
