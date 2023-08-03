eb503b7e9234977f0f9b8d9fc12a740dc0d551b2
EdgarMacharia <m.edgarmacharia@gmail.com>
2023-08-03 16:02:35 +0300

    added archive

--- db/migrate/20230725130855_create_questions.rb
+++ db/migrate/20230725130855_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[7.0]
     create_table :questions do |t|
       t.string :title
       t.text :body
+      t.boolean :archive, default: false
       t.references :user, null: false, foreign_key: true
       t.references :tag, null: false, foreign_key: true
 
