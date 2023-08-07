2658b5a0add8736cabe381d7aefb9e29aa76800e
Samuel <samnmbuguah@gmail.com>
2023-08-07 14:11:31 +0300

    Post Notifications to database

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,5 @@
 class Notification < ApplicationRecord
     belongs_to :user
     # Add associations based on the notification_type and target_id columns
-    belongs_to :target, polymorphic: true
 end
   
--- db/migrate/20230725132029_create_notifications.rb
+++ db/migrate/20230725132029_create_notifications.rb
@@ -2,7 +2,7 @@ class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
     create_table :notifications do |t|
       t.string :message
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
       t.boolean :read_status, default: false
 
       t.timestamps
