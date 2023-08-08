8a66fb841a93e5ce4429f8050ec42a61986ac669
EdgarMacharia <m.edgarmacharia@gmail.com>
2023-08-08 08:49:13 +0300

    updated tags

--- frontend/src/pages/ViewQuestion.js
+++ frontend/src/pages/ViewQuestion.js
@@ -85,14 +85,14 @@ const ViewQuestion = () => {
           <div className="w-70p sm:w-full bg-white shadow-xl rounded-lg p-3 homepage">
             <div className="flex items-center justify-between mb-2">
               <div>
-                <button
+                {/* <button
                   className="text-white hover:bg-white-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-brown-600 dark:hover:bg-brown-700 focus:outline-none dark:focus:ring-brown-800"
                   onClick={() => toggleFavorite(question.id)}
                 >
                   <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" viewBox="0 0 18 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 2h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m6 0v3H6V2m6 0a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1M5 5h8m-5 5h5m-8 0h.01M5 14h.01M8 14h5" />
                   </svg>
-                </button>
+                </button> */}
                 <Link to={`/ReportContent/${id}`}>
                 <button className="report-button">Report</button>
                 </Link>
--- frontend/src/pages/ViewTag.js
+++ frontend/src/pages/ViewTag.js
@@ -1,9 +1,122 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { QuestionsContext } from '../context/QuestionsContext';
+
 
 const ViewTag = () => {
-  return (
-    <div>ViewTag</div>
-  )
-}
+  const { tags, questions, downvoteQuestion, upvoteQuestion, toggleFavorite, isQuestionFavorited } = useContext(QuestionsContext);
+  const { id } = useParams()
 
-export default ViewTag
+const [question, setQuestion] = useState();
+
+useEffect(() => {
+  fetch(`/tags/${id}`)
+    .then((res) => res.json())
+    .then((response) => {
+      setQuestion(response.questions);
+      console.log(response.questions)
+    })
+    .catch((error) => {
+      console.error('Error fetching tag:', error);
+    });
+}, [id, tags]);
+
+  const handleUpvote = (questionId, e) => {
+    e.preventDefault();
+    upvoteQuestion(questionId);
+  };
+
+  const handleDownvote = (questionId, e) => {
+    e.preventDefault();
+    downvoteQuestion(questionId);
+  };
+
+  return (
+    <div>
+      {questions && questions.length === 0 ? (
+        <div>
+        <h1 className='text-2xl'>tags.name</h1>
+        <p>No questions found.</p>
+        </div>
+      ):(
+        
+        questions &&
+        questions.map((question, index) => {
+          return (
+            <div
+              key={question.id || index}
+              className="questions-page flex justify-center items-center bg-gray-100 sm:p-6 homepage"
+            >
+              <Link
+                to={`/ViewQuestion/${question.id}`}
+                className="w-70p sm:w-full bg-white shadow-xl rounded-lg p-3"
+              >
+                <div className="question-details p-4 bg-white rounded-lg shadow-md whitespace-normal">
+                  <h3 className="question-title text-xl font-bold mb-2">{question.title}</h3>
+                  <p className="question-details mb-4 ">{question.body}</p>
+                  <div className="vote-buttons">
+          
+                      <button
+                        onClick={(e) => handleUpvote(question.id, e)}
+                        className={`text-gray-600 hover:text-blue-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none`}
+                      >
+                        <FontAwesomeIcon
+                          icon={faThumbsUp}
+                          className={`w-5 h-5`}
+                        />
+                        <span className="ml-2"> ({question.upvotes_count} Upvotes)</span>
+                      </button>
+          
+                      <button
+                        onClick={(e) => handleDownvote(question.id, e)}
+                        className={`text-gray-600 hover:text-red-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none`}
+                      >
+                        <FontAwesomeIcon
+                          icon={faThumbsDown}
+                          className={`w-5 h-5`}
+                        />
+                        <span className="ml-2"> ( {question.downvotes_count} Downvotes)</span>
+                      </button>
+          
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          toggleFavorite(question.id);
+                        }}
+                        className={`text-gray-600 ${isQuestionFavorited(question.id) ? 'text-red-600' : 'hover:text-yellow-600'
+                          } font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none`}
+                      >
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          className={`w-5 h-5 ${isQuestionFavorited(question.id) ? 'text-red-600' : 'text-gray-600'
+                            }`}
+                        />
+                      </button>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm text-brown-500 font-medium mb-2">
+                      Answers: {question && question.answers && question.answers.length}
+                    </p>
+                    <p className="text-sm text-blue-500 font-medium mb-2">
+                      {question &&
+                        question.tag_names &&
+                        JSON.parse(question.tag_names).join(', ')}
+                    </p>
+                    <p className="text-sm text-brown-500 font-medium mb-2">
+                      {question.author_username}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default ViewTag;
