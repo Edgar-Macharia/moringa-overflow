da15168430ea888229e292b4f72b9ee1c1a3dbcd
Samuel <samnmbuguah@gmail.com>
2023-08-08 10:10:20 +0300

    Debugging

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -7,7 +7,6 @@ import { QuestionsContext } from "../context/QuestionsContext";
 
 const Navbar = () => {
   const { isLoggedIn, logout, username, currentUserData } = useContext(AuthContext);
-  console.log(currentUserData)
   const { searchQuestions, notifications } = useContext(QuestionsContext);
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -91,7 +90,7 @@ const Navbar = () => {
                       <span className="sr-only">Search icon</span>
                     </div>
                     <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."
-                      value={searchQuery} // Step 3: Bind the search query value to the input
+                      value={searchQuery}
                       onChange={handleSearchChange}
                     />
                   </div>
