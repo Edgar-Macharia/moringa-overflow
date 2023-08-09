a86ad949f17972a49330010dbef747397acd163d
Samuel <samnmbuguah@gmail.com>
2023-08-09 13:10:18 +0300

    Update  Question

--- frontend/src/pages/ViewQuestion.js
+++ frontend/src/pages/ViewQuestion.js
@@ -1,34 +1,87 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { faThumbsUp, faThumbsDown, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
-import { QuestionsContext } from '../context/QuestionsContext';
-import { AuthContext } from '../context/AuthContext';
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faTrash,
+  faEdit,
+} from "@fortawesome/free-solid-svg-icons";
+import { QuestionsContext } from "../context/QuestionsContext";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import Swal from "sweetalert2";
 
 const ViewQuestion = () => {
-
-  const { tags, updateQuestion, deleteQuestion, downvoteQuestion, upvoteQuestion, upvoteAnswer, downvoteAnswer, toggleFavorite, isQuestionFavorited, questions } = useContext(QuestionsContext)
+  const nav = useNavigate();
+  const {
+    tags,
+    deleteQuestion,
+    downvoteQuestion,
+    upvoteQuestion,
+    upvoteAnswer,
+    downvoteAnswer,
+    toggleFavorite,
+    isQuestionFavorited,
+    questions,
+  } = useContext(QuestionsContext);
   const { isLoggedIn, currentUserData } = useContext(AuthContext);
   const { id } = useParams();
 
   const [question, setQuestion] = useState();
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [title, setTitle] = useState(question?.title);
+  const [body, setBody] = useState(question?.body);
+  const [editTitle, setEditTitle] = useState(false);
+  const [editBody, setEditBody] = useState(false);
   const [userVote, setUserVote] = useState({
     question: false,
     answers: {},
   });
+
+
+    // Update a question
+    const updateQuestion = (questionId, updatedQuestionData) => {
+      const token = sessionStorage.getItem("token");
+      if (!token) {
+        Swal.fire("Error", "Not authorized to update question", "error");
+        return;
+      }
+  
+      fetch(`/questions/${questionId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ question: updatedQuestionData }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.errors) {
+            Swal.fire("Error", data.errors, "error");
+          } else {
+            console.log(data);
+            setQuestion(data);
+            Swal.fire("Success", "Question updated successfully", "success");
+            nav(`/viewquestion/${questionId}`);
+          }
+        })
+        .catch((error) => {
+          console.error("Error updating question:", error);
+          Swal.fire("Error", "Failed to update question", "error");
+        });
+    };
 
   useEffect(() => {
     fetch(`/questions/${id}`)
       .then((res) => res.json())
       .then((response) => {
         setQuestion(response);
-        console.log(response)
+        console.log(response);
       })
       .catch((error) => {
-        console.error('Error fetching question:', error);
+        console.error("Error fetching question:", error);
       });
   }, [id, questions]);
 
@@ -46,8 +99,17 @@ const ViewQuestion = () => {
     // Implement your deleteQuestion function here or call it from your context.
     deleteQuestion(questionId);
   };
+
+  const handleEditTitleClick = () => {
+    setEditTitle(true);
+  };
+
+  const handleEditBodyClick = () => {
+    setEditBody(true);
+  };
+
   const handleUpdateQuestion = (questionId) => {
-    const updatedQuestion = {}
+    const updatedQuestion = {};
     if (title !== question.title) {
       updatedQuestion.title = title;
     }
@@ -56,8 +118,8 @@ const ViewQuestion = () => {
       updatedQuestion.body = body;
     }
 
-    updateQuestion(updatedQuestion)
-  }
+    updateQuestion(questionId, updatedQuestion);
+  };
 
   const handleAnswerUpvote = (answerId) => {
     upvoteAnswer(answerId);
@@ -69,11 +131,12 @@ const ViewQuestion = () => {
 
   return (
     <>
-      <div className="flex justify-end mx-11 homepage">
-      </div>
+      <div className="flex justify-end mx-11 homepage"></div>
       {question ? (
-
-        <div className="questions-page flex justify-center items-center bg-gray-100 sm:p-6 homepage" key={question.id}>
+        <div
+          className="questions-page flex justify-center items-center bg-gray-100 sm:p-6 homepage"
+          key={question.id}
+        >
           <div className="w-70p sm:w-full bg-white shadow-xl rounded-lg p-3 homepage">
             <div className="flex items-center justify-between mb-2">
               <div>
@@ -90,44 +153,81 @@ const ViewQuestion = () => {
                 </Link>
 
                 {/* Buttons for Delete and Update */}
-                {question && question.author_username == currentUserData.username &&
-                  <div className="flex justify-end mt-4">
-                    <button
-                      className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
-                      onClick={() => handleDeleteQuestion(question.id)}
-                    >
-                      <FontAwesomeIcon icon={faTrash} className="w-5 h-5 mr-2" />
-                      Delete
-                    </button>
-                    <button
-                      className="text-white bg-yellow-700 hover:bg-yellow-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
-                      onClick={() => {
-                        handleUpdateQuestion(question.id)
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faEdit} className="w-5 h-5 mr-2" />
-                      Update
-                    </button>
-                  </div>
-                }
+                {question &&
+                  question.author_username === currentUserData.username && (
+                    <div className="flex justify-end mt-4">
+                      <button
+                        className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                        onClick={() => handleDeleteQuestion(question.id)}
+                      >
+                        <FontAwesomeIcon
+                          icon={faTrash}
+                          className="w-5 h-5 mr-2"
+                        />
+                        Delete
+                      </button>
+                      <button
+                        className="text-white bg-yellow-700 hover:bg-yellow-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                        onClick={() => {
+                          handleUpdateQuestion(question.id);
+                        }}
+                      >
+                        <FontAwesomeIcon
+                          icon={faEdit}
+                          className="w-5 h-5 mr-2"
+                        />
+                        Update
+                      </button>
+                    </div>
+                  )}
               </div>
             </div>
 
-
             <div className="question-details p-4 bg-white rounded-lg shadow-md whitespace-normal">
-              <h3 className="question-title text-xl font-bold mb-2">{question.title}</h3>
-              <p className="question-details mb-4 ">{question.body}</p>
+              {editTitle ? (
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  onBlur={() => setEditTitle(false)}
+                  className="question-title-input"
+                />
+              ) : (
+                <h3
+                  className="question-title text-xl font-bold mb-2"
+                  onClick={handleEditTitleClick}
+                >
+                  {question.title}
+                </h3>
+              )}
+              <br />
+              <br />
+              <hr />
+              {editBody ? (
+                <textarea
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                  onBlur={() => setEditBody(false)}
+                  className="question-body-textarea"
+                />
+              ) : (
+                <p
+                  className="question-details mb-4"
+                  onClick={handleEditBodyClick}
+                >
+                  {question.body}
+                </p>
+              )}
               <div className="vote-buttons">
                 {isLoggedIn && (
                   <button
                     onClick={(e) => handleUpvote(question.id, e)}
                     className={`text-gray-600 hover:text-blue-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none`}
                   >
-                    <FontAwesomeIcon
-                      icon={faThumbsUp}
-                      className={`w-5 h-5`}
-                    />
-                    <span className="ml-2">{question.upvotes_count} Upvotes</span>
+                    <FontAwesomeIcon icon={faThumbsUp} className={`w-5 h-5`} />
+                    <span className="ml-2">
+                      {question.upvotes_count} Upvotes
+                    </span>
                   </button>
                 )}
                 {isLoggedIn && (
@@ -139,7 +239,10 @@ const ViewQuestion = () => {
                       icon={faThumbsDown}
                       className={`w-5 h-5`}
                     />
-                    <span className="ml-2"> {question.downvotes_count} Downvotes</span>
+                    <span className="ml-2">
+                      {" "}
+                      {question.downvotes_count} Downvotes
+                    </span>
                   </button>
                 )}
                 {isLoggedIn && (
@@ -148,12 +251,16 @@ const ViewQuestion = () => {
                       e.preventDefault();
                       toggleFavorite(question.id);
                     }}
-                    className={`text-gray-600 ${isQuestionFavorited(question.id) ? 'text-red-600' : 'hover:text-yellow-600'
+                    className={`text-gray-600 ${isQuestionFavorited(question.id)
+                        ? "text-red-600"
+                        : "hover:text-yellow-600"
                       } font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none`}
                   >
                     <FontAwesomeIcon
                       icon={faHeart}
-                      className={`w-5 h-5 ${isQuestionFavorited(question.id) ? 'text-red-600' : 'text-gray-600'
+                      className={`w-5 h-5 ${isQuestionFavorited(question.id)
+                          ? "text-red-600"
+                          : "text-gray-600"
                         }`}
                     />
                   </button>
@@ -161,19 +268,31 @@ const ViewQuestion = () => {
               </div>
               <div className="flex items-center justify-between">
                 <p className="text-sm text-brown-500 font-medium mb-2">
-                  Answers: {question && question.answers && question.answers.length}
+                  Answers:{" "}
+                  {question && question.answers && question.answers.length}
                 </p>
                 <p className="text-sm text-blue-500 font-medium mb-2">
                   {question && question.tag_names && (
-                    JSON.parse(question.tag_names).map((tagName, index) => (
-                      <Link
-                        key={index}
-                        to={`/viewTag/${tags.find(tag => tag.name === tagName).id}`}
-                        className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-                      >
-                        {tagName}
-                      </Link>
-                    ))
+                    JSON.parse(question.tag_names).map((tagName, index) => {
+                      const foundTag = tags.find((tag) => tag.name === tagName);
+
+                      return foundTag ? (
+                        <Link
+                          key={index}
+                          to={`/viewTag/${foundTag.id}`}
+                          className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+                        >
+                          {tagName}
+                        </Link>
+                      ) : (
+                        <span
+                          key={index}
+                          className="text-red-500 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                        >
+                          Tag Not Found: {tagName}
+                        </span>
+                      );
+                    })
                   )}
                 </p>
                 <p className="text-sm text-brown-500 font-medium mb-2">
@@ -186,32 +305,47 @@ const ViewQuestion = () => {
               <h4 className="text-lg font-bold">Answers:</h4>
               {question.answers &&
                 question.answers.map((answer) => (
-                  <div key={answer.id} className="answer p-4 bg-white rounded-lg shadow-md mt-4">
-                    <p>{answer.body}</p> {/* Use answer.body to render the answer body */}
+                  <div
+                    key={answer.id}
+                    className="answer p-4 bg-white rounded-lg shadow-md mt-4"
+                  >
+                    <p>{answer.body}</p>{" "}
+                    {/* Use answer.body to render the answer body */}
                     <div className="answer-meta mt-2">
                       <span>By {answer.author}</span>
                       <div className="vote-buttons mt-2">
                         <button
-                          className={`text-gray-600 hover:text-blue-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none ${userVote.answers[answer.id] ? 'pointer-events-none opacity-50' : ''
+                          className={`text-gray-600 hover:text-blue-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none ${userVote.answers[answer.id]
+                              ? "pointer-events-none opacity-50"
+                              : ""
                             }`}
                           onClick={() => handleAnswerUpvote(answer.id)}
                         >
                           <FontAwesomeIcon
                             icon={faThumbsUp}
-                            className={`w-5 h-5 ${userVote.answers[answer.id] ? 'text-blue-600' : ''}`}
+                            className={`w-5 h-5 ${userVote.answers[answer.id] ? "text-blue-600" : ""
+                              }`}
                           />
-                          <span className="ml-2"> {answer.upvotes_count}</span> {/* Use answer.upvotes */}
+                          <span className="ml-2"> {answer.upvotes_count}</span>{" "}
+                          {/* Use answer.upvotes */}
                         </button>
                         <button
-                          className={`text-gray-600 hover:text-red-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none ${userVote.answers[answer.id] ? 'pointer-events-none opacity-50' : ''
+                          className={`text-gray-600 hover:text-red-600 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 focus:outline-none ${userVote.answers[answer.id]
+                              ? "pointer-events-none opacity-50"
+                              : ""
                             }`}
                           onClick={() => handleAnswerDownvote(answer.id)}
                         >
                           <FontAwesomeIcon
                             icon={faThumbsDown}
-                            className={`w-5 h-5 ${userVote.answers[answer.id] ? 'text-red-600' : ''}`}
+                            className={`w-5 h-5 ${userVote.answers[answer.id] ? "text-red-600" : ""
+                              }`}
                           />
-                          <span className="ml-2"> {answer.downvotes_count}</span> {/* Use answer.downvotes */}
+                          <span className="ml-2">
+                            {" "}
+                            {answer.downvotes_count}
+                          </span>{" "}
+                          {/* Use answer.downvotes */}
                         </button>
                       </div>
                     </div>
@@ -221,7 +355,7 @@ const ViewQuestion = () => {
           </div>
         </div>
       ) : (
-        <p className='ml-10'>Couldn't find Question...</p>
+        <p className="ml-10">Couldn't find Question...</p>
       )}
     </>
   );
