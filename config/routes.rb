902953ca81e422a73fc21f7ec0b01ccce8ecf155
Samuel <samnmbuguah@gmail.com>
2023-08-10 05:50:26 +0300

    Debugging password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,22 @@
 Rails.application.routes.draw do
-  # namespace :api do
-  #   resources :notifications, only: [:index]
-  #   patch 'notifications/mark_as_read/:id', to: 'notifications#mark_as_read'
-  # end
-   post '/api/password-reset', to: 'reset#request_reset'
-   post '/password-reset', to: 'reset#request_reset'
-   put '/PasswordResetPage/:reset_token', to: 'reset#reset_password', as: 'password_reset_page'
-  # get '/api/password-reset', to: 'reset#reset_password', as: :reset_password
-  # get '/PasswordResetPage/:reset_token', to: 'reset#reset_password', as: 'password_reset_page'
-  # get 'api/password-reset', to: 'reset#reset_password', as: :password_reset_page
-  put 'api/password-reset/:reset_token', to: 'reset#reset_password', as: :reset_password
-  resources :notifications, only: [:index, :update]
-  resources :reported_contents
-  resources :comments
-  resources :answers do
+  # Defines the root path route ("/")
+
+  resources :users, only: [:create, :show, :update] do
     member do
-      post 'upvote', to: 'answers#upvote'
-      post 'downvote', to: 'answers#downvote'
+      get 'favorite_questions', to: 'users#favorite_questions'
+      put :reset_password
+      post 'update_profile_picture'
     end
   end
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#create'
+  post '/api/password-reset', to: 'reset#request_reset'
+  put '/PasswordResetPage/:reset_token', to: 'reset#reset_password', as: 'password_reset_page'
 
-  resources :tags
+  resources :notifications, only: [:index, :update]
+  resources :reported_contents, only: [:index, :create, :destroy]
 
+  resources :tags, only: [:index, :show, :create,]
   resources :questions do
     collection do
       get 'search', to: 'questions#search'
@@ -34,27 +29,14 @@ Rails.application.routes.draw do
       post 'favorite', to: 'questions#favorite'
     end
   end
-  
-  resources :users, only: [:create, :show, :update] do
-    member do
-      get 'favorite_questions', to: 'users#favorite_questions'
-      put :reset_password
-      post 'update_profile_picture'
-    end
-  end
 
-
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#create'
   get "/questions/search", to: "questions#search"
   delete '/questions', to: 'questions#destroy'
-  
-  
-  # Defines the root path route ("/")
-  # root "articles#index"
 
-  # namespace :api do
-  #   resources :notifications, only: [:index]
-  #   patch 'notifications/mark_as_read/:id', to: 'notifications#mark_as_read', as: :mark_notification_as_read
-  # end
+  resources :answers do
+    member do
+      post 'upvote', to: 'answers#upvote'
+      post 'downvote', to: 'answers#downvote'
+    end
+  end
 end
